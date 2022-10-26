97654455575b4dad5d9f794d8dfda4d5b29a1b47
Christian Bækdorf <Christian.Baekdorf@3shape.com>
2022-10-26 14:33:07 +0200

    hgfhgfh

--- dist/index.js
+++ dist/index.js
@@ -9688,14 +9688,27 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 async function run() {
-    console.log('Hello from my git action one! v2');
+    console.log('Hello from my git action one! v3');
 }
 
 try {
+
+    console.log("aaaaaaaaaaaaaaaa");
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    console.log("bbbbbbbbbbbbbbbbbb");
+
+    // do then other stuff
     run();
+
+
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+
+
+
 }
 catch (error)
 {
