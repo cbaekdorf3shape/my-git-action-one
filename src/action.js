bb09d61f6487159366d91f658ef5eda2d1a023f6
Christian Bækdorf <Christian.Baekdorf@3shape.com>
2022-10-26 13:30:08 +0200

    git npm import

--- src/action.js
+++ src/action.js
@@ -1,5 +1,8 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
-    console.log('Hello from my git action one!');
+    console.log('Hello from my git action one! v2');
 }
 
 run();
