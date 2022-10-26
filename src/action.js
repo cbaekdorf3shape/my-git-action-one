8f9408eb1aa4f65e2b0308d1588394af19ee4def
Christian Bækdorf <Christian.Baekdorf@3shape.com>
2022-10-26 14:27:41 +0200

    sfsfsf

--- src/action.js
+++ src/action.js
@@ -2,20 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    console.log('Hello from my git action one! v2');
+    console.log('Hello from my git action one! v3');
 }
 
 try {
+
+    console.log("aaaaaaaaaaaaaaaa");
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    console.log("bbbbbbbbbbbbbbbbbb");
+
+    // do then other stuff
+    run();
+
+
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
 
-    // do then other stuff
-    run();
 
 }
 catch (error)
